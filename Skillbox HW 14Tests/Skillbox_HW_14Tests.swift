4240c2fab61f01ce1e5dc7b37caa71b65b7734fa
Nikita Selianin <rhineford@icloud.com>
2020-08-28 01:06:19 +0300

    Initial Commit

---
+++ Skillbox HW 14Tests/Skillbox_HW_14Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Skillbox_HW_14Tests.swift
+//  Skillbox HW 14Tests
+//
+//  Created by Nikita Selianin on 28.08.2020.
+//  Copyright © 2020 Nikita Selianin. All rights reserved.
+//
+
+import XCTest
+@testable import Skillbox_HW_14
+
+class Skillbox_HW_14Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
